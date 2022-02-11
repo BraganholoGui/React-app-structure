0f791500e30007db3b5bdce6e33ef2bcdf1d41dd
GuilhermeBraganholo <guilherme@cleandev@.com.br>
2022-02-11 15:15:03 -0300

    The initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HeaderInit from './Header/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderInit></HeaderInit>
+    </div>
+  );
+}
+
+export default App;
