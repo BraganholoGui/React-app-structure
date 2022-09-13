97c52c3519e3bb09e979fe77d88d39afac239105
Guilherme Braganholo <guilherme@cleandev.com.br>
2022-09-13 17:39:01 -0300

    feat: add some style for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,18 @@
-import './style.css';
+import * as S from './style';
 import face from '../../static/Logo/face.png';
 import insta from '../../static/Logo/insta.png';
 import mail from '../../static/Logo/mail.png';
 import React from 'react';
 
-function HeaderInit(){
-  return(
-    <div>
-      header
-      <hr/>
-    </div>
-    
+function HeaderInit() {
+  return (
+    <>
+      <S.ContainerMain>
+        Header
+      </S.ContainerMain>
+    </>
   )
-    
+
 }
 
 export default HeaderInit;
